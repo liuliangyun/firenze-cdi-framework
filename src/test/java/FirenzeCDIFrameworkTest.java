561cc061686fc306207d416de2a59a9d342db4b6
lyliu <lyliu@thoughtworks.com>
2021-09-01 13:22:20 +0800

    [lyliu] refactor fixture code

--- src/test/java/FirenzeCDIFrameworkTest.java
+++ src/test/java/FirenzeCDIFrameworkTest.java
@@ -1,109 +1,85 @@
 import com.thoughtworks.fusheng.integration.junit5.FuShengTest;
-import exceptions.CircularDependencyException;
-import exceptions.InterfaceInjectException;
 import testCases.circularDependency.MovieListerAInjectB;
 import testCases.injectClassDependency.MovieListerInjectClassDependency;
 import testCases.injectInterfaceDependency.*;
 import testCases.withoutDependency.MovieListerWithoutDependency;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.util.Objects.isNull;
 
 @FuShengTest
 public class FirenzeCDIFrameworkTest {
     private Container container;
+    private Map<String, Class> implementationMap = new HashMap<>(){{
+        put("ColonDelimitedMovieFinder", ColonDelimitedMovieFinder.class);
+        put("DatabaseMovieFinder", DatabaseMovieFinder.class);
+    }};
 
     public void newContainer () {
         container = new FirenzeContainer();
     }
 
     public void registerImplementation (String implementation) {
-        if ("ColonDelimitedMovieFinder".equals(implementation)) {
-            container.registerImplementation(MovieFinderInterface.class, ColonDelimitedMovieFinder.class);
-        } else if ("DatabaseMovieFinder".equals(implementation)) {
-            container.registerImplementation(MovieFinderInterface.class, DatabaseMovieFinder.class);
-        }
+        container.registerImplementation(MovieFinderInterface.class, implementationMap.get(implementation));
     }
 
     public boolean isSuccessGetComponent (String dependency) {
         if ("没有依赖".equals(dependency)) {
-            MovieListerWithoutDependency lister = null;
-            try {
-                lister = (MovieListerWithoutDependency)
-                        container.getComponent(MovieListerWithoutDependency.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerWithoutDependency lister = getComponentWithoutDependency();
             return !isNull(lister);
         } else if ("class".equals(dependency)){
-            MovieListerInjectClassDependency lister = null;
-            try {
-                lister = (MovieListerInjectClassDependency)
-                        container.getComponent(MovieListerInjectClassDependency.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerInjectClassDependency lister = getComponentForInjectClass();
             return !isNull(lister) && !isNull(lister.finder);
         } else if ("interface".equals(dependency)) {
-            MovieListerInjectInterfaceDependency lister = null;
-            try {
-                lister = (MovieListerInjectInterfaceDependency)
-                        container.getComponent(MovieListerInjectInterfaceDependency.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerInjectInterfaceDependency lister = getComponentForInjectInterface();
             return !isNull(lister) && !isNull(lister.finder);
         } else if ("@Named(\'data\') interface".equals(dependency)) {
-            MovieListerInjectNotExistedNamedInterfaceDependency lister = null;
-            try {
-                lister = (MovieListerInjectNotExistedNamedInterfaceDependency)
-                        container.getComponent(MovieListerInjectNotExistedNamedInterfaceDependency.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerInjectNotExistedNamedInterfaceDependency lister = getComponentForInjectInterfaceWithNotExistedName();
             return !isNull(lister) && !isNull(lister.finder);
         } else if ("@Named(\'database\') interface".equals(dependency)) {
-            MovieListerInjectNamedInterfaceDependency lister = null;
-            try {
-                lister = (MovieListerInjectNamedInterfaceDependency)
-                        container.getComponent(MovieListerInjectNamedInterfaceDependency.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerInjectNamedInterfaceDependency lister = getComponentForInjectInterfaceWithExistedName();
             return !isNull(lister) && !isNull(lister.finder);
         } else if ("相互依赖".equals(dependency)) {
-            MovieListerAInjectB lister = null;
-            try {
-                lister = (MovieListerAInjectB)
-                        container.getComponent(MovieListerAInjectB.class);
-            } catch (InterfaceInjectException e) {
-                e.printStackTrace();
-            } catch (CircularDependencyException e) {
-                e.printStackTrace();
-            }
+            MovieListerAInjectB lister = getComponentForInjectCircular();
             return !isNull(lister);
         }
         return false;
     }
 
-    public MovieListerWithoutDependency getComponentForClassWithoutDependency () {
-        MovieListerWithoutDependency lister = null;
+    public Object getComponent (Class clazz) {
+        Object lister = null;
         try {
-            lister = (MovieListerWithoutDependency) container.getComponent(MovieListerWithoutDependency.class);
-        } catch (InterfaceInjectException e) {
-            e.printStackTrace();
-        } catch (CircularDependencyException e) {
-            e.printStackTrace();
+            lister = container.getComponent(clazz);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         return lister;
+    }
+
+    public MovieListerWithoutDependency getComponentWithoutDependency () {
+        return (MovieListerWithoutDependency) getComponent(MovieListerWithoutDependency.class);
+    }
+
+    public MovieListerInjectClassDependency getComponentForInjectClass () {
+        return (MovieListerInjectClassDependency) getComponent(MovieListerInjectClassDependency.class);
+    }
+
+    public MovieListerInjectInterfaceDependency getComponentForInjectInterface () {
+        return (MovieListerInjectInterfaceDependency) getComponent(MovieListerInjectInterfaceDependency.class);
+    }
+
+    public MovieListerInjectNotExistedNamedInterfaceDependency getComponentForInjectInterfaceWithNotExistedName () {
+        return (MovieListerInjectNotExistedNamedInterfaceDependency) getComponent(MovieListerInjectNotExistedNamedInterfaceDependency.class);
+    }
+
+    public MovieListerInjectNamedInterfaceDependency getComponentForInjectInterfaceWithExistedName () {
+        return (MovieListerInjectNamedInterfaceDependency) getComponent(MovieListerInjectNamedInterfaceDependency.class);
+    }
+
+    public MovieListerAInjectB getComponentForInjectCircular () {
+        return (MovieListerAInjectB) getComponent(MovieListerAInjectB.class);
     }
 
     public boolean isSameComponent (Object component1, Object component2) {
